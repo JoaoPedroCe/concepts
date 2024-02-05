3abe72c0084dba4b308e64fb9992f723c3a97389
joao pedro <joao@gmail.com>
2024-02-04 22:37:32 -0300

    feat - add

---
+++ src/app/useEffectExercises/Exercise1/page.tsx
@@ -0,0 +1,4 @@
+"use client";
+export default function Exercise1() {
+  <div>Exercicio 1</div>;
+}
---
+++ src/app/useEffectExercises/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function UseEffectExercises() {
+  return (
+    <ul>
+      <li>
+        <Link href="/Exercise1">Exercise1</Link>
+      </li>
+    </ul>
+  );
+}
