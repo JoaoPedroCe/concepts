1a936fd660916a04d04977bbcede5bf50f3fe42c
joao pedro <joao@gmail.com>
2024-02-04 22:53:40 -0300

    feat - add title

--- src/app/useEffectExercises/Exercise1/page.tsx
+++ src/app/useEffectExercises/Exercise1/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ExerciseTitle from "@/components/Title/ExerciseTitle";
 import { useEffect, useState } from "react";
 
 export function createConnection(serverUrl: string, roomId: string) {
@@ -40,6 +41,11 @@ export default function App() {
   const [roomId, setRoomId] = useState("general");
   return (
     <>
+      <ExerciseTitle
+        title="Challenge 1 of 5: Fix reconnecting on every keystroke"
+        description="In this example, the ChatRoom component connects to the chat room when the component mounts, disconnects when it unmounts, and reconnects when you select a different chat room. This behavior is correct, so you need to keep it working.
+However, there is a problem. Whenever you type into the message box input at the bottom, ChatRoom also reconnects to the chat. (You can notice this by clearing the console and typing into the input.) Fix the issue so that this doesnt happen."
+      />
       <label>
         Choose the chat room:{" "}
         <select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
